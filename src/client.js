797dd813ccdcb947a4e72ee7987e516217bf3f33
Jenna Tucker <jenna.tucker@tozny.com>
2019-09-19 16:13:08 -0700

    dev

--- src/client.js
+++ src/client.js
@@ -16,27 +16,20 @@ class Client {
   }
 
   async changePassword({ password, newPassword }) {
-    console.log("password", password)
-    console.log("newPassword", newPassword)
-    console.log(this._queenClient.crypto)
+    const passwordChecksOut = await this.validatePassword(password)
+    if (passwordChecksOut) {
+      console.log("Add call to change password")
+    } else {
+      return new Error('Current password incorrect.')
+    }
+  }
+
+  async validatePassword(password) {
     const crypto = this._queenClient.crypto
-    console.log(crypto)
     const authSalt = await crypto.b64decode(this.profile.auth_salt)
-    console.log('authSalt', authSalt)
     const keypair = await crypto.deriveSigningKey(password, authSalt, KEY_HASH_ROUNDS)
-    console.log('keypair publicKey', keypair.publicKey)
     const signingKey = this.profile.signing_key
-    console.log('signingKey', signingKey.ed25519)
-    return 'THE RETURN STATEMENT'
-    // verifyPassword(maybePassword) {
-    //   return new Promise((resolve, reject) => {
-    //     let authSalt = helpers.b64decode(sessionStorage.getItem('salt.auth'))
-    //     let keypair = toznyCrypto.deriveSigningKey(maybePassword, authSalt)
-    //     let attempt = helpers.b64encode(keypair.publicKey)
-    //     if (sessionStorage.getItem('key.signing') === attempt) resolve()
-    //     else reject(new Error('Your password is incorrect.'))
-    //   })
-    // }
+    return keypair.publicKey === signingKey.ed25519
   }
 
   async billingStatus() {
