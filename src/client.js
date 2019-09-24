9b05c9e52a8839d341cadca73c9300e55de89618
Jenna Tucker <jenna.tucker@tozny.com>
2019-09-24 09:26:53 -0700

    throw error

--- src/client.js
+++ src/client.js
@@ -25,31 +25,19 @@ class Client {
 
   async changePassword({ password, newPassword }) {
     const currentProfileMeta = await this.api.getProfileMeta()
-    console.log('currentProfileMeta', currentProfileMeta)
     const passwordChecksOut = await this.validatePassword(password)
     const crypto = this._queenClient.crypto
-    // const paperEncKey = await crypto.b64decode(this.profile.paper_enc_salt)
-    console.log('crypto', crypto)
     if (passwordChecksOut) {
-      console.log("in if statement")
       // Generate new salts and keys
       const serializedQueenClientConfig = this._queenClient.config.serialize()
-      // console.log(queenClientConfig)
-      // console.log(JSON.stringify(queenClientConfig))
       const encSalt = await crypto.randomBytes(16)
-      console.log('encSalt', encSalt)
       const authSalt = await crypto.randomBytes(16)
-      console.log('authSalt', authSalt)
       const encKey = await crypto.deriveSymmetricKey(
         newPassword,
         encSalt,
         KEY_HASH_ROUNDS)
-      console.log('encKey', encKey)
       const authKeypair = await crypto.deriveSigningKey(newPassword, authSalt, KEY_HASH_ROUNDS)
-      console.log('authKeyPair', authKeypair)
       const encQueenCreds = await crypto.encryptString(JSON.stringify(serializedQueenClientConfig), encKey)
-      // const paperEncQueenCreds = await crypto.encryptString(JSON.stringify(serializedQueenClientConfig), paperEncKey)
-      console.log('encQueenCreds', encQueenCreds)
       const b64AuthSalt = await crypto.b64encode(authSalt)
       const b64EncSalt = await crypto.b64encode(encSalt)
       const newProfileInfo = {
@@ -61,41 +49,15 @@ class Client {
       }
 
       const updateProfileResponse = await this.api.updateProfile(newProfileInfo)
-      console.log('updateProfileResponse', updateProfileResponse)
       const updateProfileMetaResponse = await this.api.updateProfileMeta({
         backupEnabled: currentProfileMeta.backupEnabled,
         backupClient: encQueenCreds,
         paperBackup: currentProfileMeta.paperBackup
       })
       return this.api.getProfileMeta()
-      // console.log("Add call to change password")
-      // console.log('encSalt', this.profile.enc_salt)
-      // const encSalt = crypto.b64decode(this.profile.enc_salt)
-      // console.log('encKeys', encKeys)
-      // const encKey = crypto.deriveCryptoKey(password, encSalt)
-      // console.log('')
-      // changePassword = (old, password) => {
-      //   // Derive the old keys
-      //   const oldEncSalt = helpers.b64decode(sessionStorage.getItem('salt.enc'))
-      //   const oldEncKey = toznyCrypto.deriveCryptoKey(old, oldEncSalt)
-
-      //   // Fetch and decrypt the user's backup credentials
-      //   const raw = sessionStorage.getItem('creds.backup')
-      //   const nonce = helpers.b64decode(raw.split(':')[0])
-      //   const cipher = helpers.b64decode(raw.split(':')[1])
-      //   const jsonConfig = sodium.crypto_secretbox_open_easy(
-      //     cipher,
-      //     nonce,
-      //     oldEncKey
-      //   )
-
-      //   return this.setNewPassword(password, jsonConfig)
-      // }
-
 
     } else {
-      console.log('in else statement')
-      return new Error('Current password incorrect.')
+      throw new Error('Current password incorrect.')
     }
   }
 
