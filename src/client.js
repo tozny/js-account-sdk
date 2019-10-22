247a5498952ddbde728a0bcc8bcb80ad717ae868
Jenna Tucker <jenna.tucker@tozny.com>
2019-10-22 14:52:33 -0700

    with logs

--- src/client.js
+++ src/client.js
@@ -37,7 +37,7 @@ class Client {
 
   async changePassword({ password, newPassword, type = 'standard' }) {
     console.log('calls sdk changePassword in client')
-    /* 
+    /*
      *  If the type is paper, the password is the user's backup paperkey.
      *  In this case, the paperkey has already been validated.
      */
