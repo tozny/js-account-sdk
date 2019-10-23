492c4d5c1ca2788c284dcc0dc9822673fff31ed6
Jenna Tucker <jenna.tucker@tozny.com>
2019-10-23 16:24:20 -0700

    logs

--- src/api/token.js
+++ src/api/token.js
@@ -15,6 +15,9 @@ class Token {
   }
 
   get expired() {
+    console.log(this._created)
+    console.log(TOKEN_LIFETIME_SECONDS)
+    console.log(Date.now() - this._created > TOKEN_LIFETIME_SECONDS)
     return Date.now() - this._created > TOKEN_LIFETIME_SECONDS
   }
 
