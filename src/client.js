2958ed1917c96a021e4036afa2e54ec72ec08222
Jenna Tucker <jenna.tucker@tozny.com>
2019-10-30 13:49:45 -0700

    clean up

--- src/client.js
+++ src/client.js
@@ -342,9 +342,17 @@ class Client {
     return Identity.decode(rawResponse)
   }
 
+  /*
+    refreshProfile users internal logic in the api token refresher
+    to update the user's profile info from the backend. 
+    Currently, this is used to allow a user to verify their email,
+    hit refresh in an already open window, and continue with an
+    updated accountClient on the frontend.  
+
+    This will likely be replaced by a call to GET the account profile.
+  */
+
   async refreshProfile() {
-    console.log(this.api)
-    
     const fetched = await this.api._token._refresher.profile()
     this.account = fetched.account
     this.profile = fetched.profile
