b362febfd1582b8bc3a59146fa52e23d9a45e64b
Katie Rosas <katie@tozny.com>
2021-08-06 09:13:11 -0700

    [WR-113] Group Membership

--- src/__tests__/groupRoleMappings.test.ts
+++ src/__tests__/groupRoleMappings.test.ts
@@ -5,6 +5,9 @@ import { v4 as uuidv4 } from 'uuid'
 import { cleanupRealms } from './utils'
 import { Role } from '../types'
 
+// default of 5s is sometimes, but not always, enough time. give the tests 10s
+jest.setTimeout(100000)
+
 const accountFactory = new Account(Tozny, process.env.API_URL)
 let client: any = null
 let realmName: string
---
+++ src/api/groupMembership.ts
@@ -0,0 +1,82 @@
+import { validateRequestAsJSON, checkStatus} from '../utils'
+import { APIContext } from './context'
+import { ToznyAPIGroup } from '../types/group'
+
+type GroupMembershipData = {
+  realmName: string
+  identityId: string
+}
+
+type GroupMembershipResponse = {groups: ToznyAPIGroup[]}
+export async function groupMembership(
+  { realmName, identityId }: GroupMembershipData,
+  { apiUrl, queenClient }: APIContext
+): Promise<ToznyAPIGroup[]> {
+
+  const response = await queenClient.authenticator.tsv1Fetch(
+    `${apiUrl}/v1/identity/realm/${realmName}/identity/${identityId}/groups`,
+    {
+      method: 'GET',
+    }
+  )
+  const { groups } = (await validateRequestAsJSON(response)) as GroupMembershipResponse
+return groups
+}
+
+type UpdateGroupMembershipData = {
+	realmName: string
+	identityId: string
+	groups: ToznyAPIGroup[]}
+export async function updateGroupMembership(
+  { realmName, identityId, groups }: UpdateGroupMembershipData,
+  { apiUrl, queenClient }: APIContext
+): Promise<void> {
+  const response = await queenClient.authenticator.tsv1Fetch(
+    `${apiUrl}/v1/identity/realm/${realmName}/identity/${identityId}/groups`,
+	  {
+		  method: 'PUT',
+		  body: JSON.stringify(groups),
+
+	  }
+  )
+    checkStatus(response)
+ return 
+}
+
+type JoinGroupsData = {
+	realmName: string
+	identityId: string
+	groups: ToznyAPIGroup[]} 
+export async function joinGroups(
+  { realmName, identityId, groups }: JoinGroupsData,
+  { apiUrl, queenClient }: APIContext
+): Promise<void> {
+  const response = await queenClient.authenticator.tsv1Fetch(
+     `${apiUrl}/v1/identity/realm/${realmName}/identity/${identityId}/groups`,
+	  {
+		  method: 'PATCH',
+		  body: JSON.stringify(groups),
+	  }
+  )
+     checkStatus(response)
+ return 
+}
+
+type LeaveGroupsData = {
+	realmName: string
+	identityId: string
+	groups: ToznyAPIGroup[]} 
+export async function leaveGroups(
+  { realmName, identityId, groups }: LeaveGroupsData,
+  { apiUrl, queenClient }: APIContext
+): Promise<void> {
+  const response = await queenClient.authenticator.tsv1Fetch(
+     `${apiUrl}/v1/identity/realm/${realmName}/identity/${identityId}/groups`,
+	  {
+		  method: 'DELETE',
+		   body: JSON.stringify(groups),
+	  }
+  )
+      checkStatus(response)
+ return 
+}
