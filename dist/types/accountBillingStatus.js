46e3d9f06e9fd3af1793f7bb4bd89274abc6ff3f
Alannah Carr <alannah@tozny.com>
2021-11-18 11:52:23 -0500

    [PLAT788] Add isFree to billing status

--- dist/types/accountBillingStatus.js
+++ dist/types/accountBillingStatus.js
@@ -3,12 +3,13 @@
  * Billing status for a Tozny account
  */
 class AccountBillingStatus {
-    constructor(accountActive, isTrial, trialPeriodEnds, isLegacy, isGoodStanding = null) {
+    constructor(accountActive, isTrial, trialPeriodEnds, isLegacy, isGoodStanding = null, isFree = null) {
         this.accountActive = accountActive;
         this.isTrial = isTrial;
         this.trialPeriodEnds = trialPeriodEnds;
         this.isLegacy = isLegacy;
         this.isGoodStanding = isGoodStanding;
+        this.isFree = isFree;
     }
     /**
      * Specify how an already unserialized JSON array should be marshaled into
@@ -20,7 +21,8 @@ class AccountBillingStatus {
      *  is_trial: true,
      *  trial_period_ends: "2019-09-22T05:19:52Z",
      *  is_legacy: false,
-     *  is_good_standing: true
+     *  is_good_standing: true,
+     *  is_free: false,
      * })
      * <code>
      *
@@ -29,7 +31,7 @@ class AccountBillingStatus {
      * @return {<AccountBillingStatus>}
      */
     static decode(json) {
-        return new AccountBillingStatus(json.account_active, json.is_trial, json.trial_period_ends, json.is_legacy, json.is_good_standing);
+        return new AccountBillingStatus(json.account_active, json.is_trial, json.trial_period_ends, json.is_legacy, json.is_good_standing, json.is_free);
     }
 }
 module.exports = AccountBillingStatus;
